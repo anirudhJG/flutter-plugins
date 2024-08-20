f42bed0459890c09f8e1e206e9bcfb7efc279d40
Anirudh Muralidhar <anirudh@jungleegames.com>
2024-08-20 14:57:45 +0530

    removed java config

--- packages/desktop_webview_window/macos/Classes/WebViewLayoutController.swift
+++ packages/desktop_webview_window/macos/Classes/WebViewLayoutController.swift
@@ -87,7 +87,6 @@ class WebViewLayoutController: NSViewController {
     webView.uiDelegate = self
 
     // TODO(boyan01) Make it configuable from flutter.
-    webView.configuration.preferences.javaEnabled = false
     webView.configuration.preferences.minimumFontSize = 12
     webView.configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
     webView.configuration.allowsAirPlayForMediaPlayback = true
